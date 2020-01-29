097d1a5fa432a335a25ac2dd1c4336e8387efd52
Louise Mpili <louisempili@macbook-pro-de-louise.home>
2020-01-29 17:25:39 +0100

    add email

--- index.php
+++ index.php
@@ -81,7 +81,13 @@ if (isset($_POST['submit']) && !empty($_POST['username']) && !empty($_POST['pass
 						<span class="focus-input100"></span>
 					</div>
 
-					<div class="flex-sb-m w-full p-b-30">
+					<div class="wrap-input100 validate-input m-b-26" data-validate="Username is required">
+						<span class="label-input100">Email</span>
+						<input class="input100" type="email" name="email" placeholder="Enter email">
+						<span class="focus-input100"></span>
+					</div>
+
+					<!-- <div class="flex-sb-m w-full p-b-30">
 						<div class="contact100-form-checkbox">
 							<input class="input-checkbox100" id="ckb1" type="checkbox" name="remember-me">
 							<label class="label-checkbox100" for="ckb1">
@@ -89,12 +95,12 @@ if (isset($_POST['submit']) && !empty($_POST['username']) && !empty($_POST['pass
 							</label>
 						</div>
 
-						<!-- <div>
+					     <div>
 							<a href="#" class="txt1">
 								Forgot Password?
 							</a>
-						</div> -->
-					</div>
+						</div> 
+					</div> -->
 
 					<div class="container-login100-form-btn">
 						<button class="login100-form-btn" name="submit">
